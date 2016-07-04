13fbe270384a54ece85cec762e2ed1cd76ac6097
Jacob Killelea <Jkillelea@protonmail.ch>
2016-07-04 01:21:39 -0700

    change uninstall order

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Uninstalling ruby dependencies..."
-sudo gem uninstall i2c
-sudo gem uninstall rmagick
 sudo gem uninstall SSD1306
+sudo gem uninstall rmagick
+sudo gem uninstall i2c
 
 echo "Uninstalling system level dependencies..."
 sudo apt-get autoremove -y ruby \
