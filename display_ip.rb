0917911342873fae3273363af330508d5f2d7677
Jacob Killelea <Jkillelea@protonmail.ch>
2016-07-04 22:04:14 -0700

    accepts a "--font" flag

--- display_ip.rb
+++ display_ip.rb
@@ -2,12 +2,19 @@
 
 require "socket"
 require "SSD1306"
+require "./lib/argparser.rb"
+
+font_size = ArgParser::val_at_flag "--font"
+
 
 disp = SSD1306::Display.new
 
-disp.font_size 2
-# disp.println "IP Address:"
-# disp.print Socket.ip_address_list[1].ip_address
+unless font_size == nil
+  disp.font_size == font_size
+else
+  disp.font_size == 1
+end
+
 ip = Socket.ip_address_list[1].ip_address
 ip_array = ip.split "."
 ip_array.each_with_index { |e, i|
