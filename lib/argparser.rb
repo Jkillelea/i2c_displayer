1a704ef1a40dfc526ac58d9dce355b4184132cc0
Jacob Killelea <Jkillelea@protonmail.ch>
2016-07-04 22:26:56 -0700

    misc changes

--- lib/argparser.rb
+++ lib/argparser.rb
@@ -5,15 +5,16 @@ module ArgParser
   end
 
   def self.find_arg_val(flag) # returns the next after a specified flag string, such as "-t"
-    return nil if ARGV.empty?
-    
-    ARGV.each_with_index do |arg, index|
+    return nil if ARGV.empty? # if there's no args, prevent returning of empty array
+
+    ARGV.each_index do |index|
       if ARGV[index] == flag
         return ARGV[index+1]
       else
         return nil
       end
     end
+
   end
 
   def self.val_at_flag(flag) # just calls the function above
