9359687d806d16e116cb9722e78488b2663c41fa
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-08 00:30:36 -0700

    checks that --font was actually supplied

--- display_stdin.rb
+++ display_stdin.rb
@@ -5,17 +5,20 @@ require "./lib/argparser"
 
 disp = SSD1306::Display.new
 
-
-fs = (ArgParser::val_at_flag( "--font")).to_i # get val at '--font' flag
-begin
-  if(fs >= 1 && fs <= 2 && fs.is_a?(Integer))   # must be either 1 or 2, and an int
-    disp.font_size fs
-  else                             # if it's not...
-    raise "FONT SIZE OUT OF BOUNDS :: reverting to font size 1 (smallest)"
+if ArgParser::given_arg? "--font"
+  fs = (ArgParser::val_at_flag( "--font")).to_i # get val at '--font' flag
+  begin
+    if(fs >= 1 && fs <= 2 && fs.is_a?(Integer))   # must be either 1 or 2, and an int
+      disp.font_size fs
+    else                             # if it's not...
+      raise "FONT SIZE OUT OF BOUNDS :: reverting to font size 1 (smallest)"
+    end
+  rescue RuntimeError => raisedError # let me know
+    disp.font_size 1
+    STDERR.puts raisedError.message
   end
-rescue RuntimeError => raisedError # let me know
+else
   disp.font_size 1
-  STDERR.puts raisedError.message
 end
 
 
