8441c95fd44b58fcc41a4db69d9fa3e87f4567a7
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-07 23:10:06 -0700

    protects against font sizes other than 1 or 2

--- display_stdin.rb
+++ display_stdin.rb
@@ -6,7 +6,11 @@ require "./lib/argparser"
 disp = SSD1306::Display.new
 
 fs = (ArgParser::val_at_flag( "--font")).to_i
-disp.font_size fs 
+if fs >= 1 && fs <= 2
+  disp.font_size fs
+else
+  disp.font_size 2 
+end
 
 while to_display = STDIN.getc do
   disp.print to_display
