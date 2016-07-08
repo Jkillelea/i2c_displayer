f9b2dd75331427399a8144d69f45aa058213d78d
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-08 11:40:47 -0700

    Revert "that should be an 'if', not an 'unless'."
    This reverts commit 3b9736a33aafa67f8ec8b3f04b1c2ce158335b3d.

--- display_stdin.rb
+++ display_stdin.rb
@@ -6,7 +6,7 @@ require "./lib/argparser"
 disp = SSD1306::Display.new
 
 FONT_SIZE  = ArgParser::val_at_flag "--font"
-if FONT_SIZE == nil
+unless FONT_SIZE == nil
   disp.font_size = FONT_SIZE.to_i
 else
   disp.font_size = 1
