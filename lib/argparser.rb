1fc225210c7007ef855dbe4ef1be3a4b152bd042
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-07 23:54:29 -0700

    given_flag? function added + documentation inside

--- lib/argparser.rb
+++ lib/argparser.rb
@@ -4,11 +4,10 @@ module ArgParser
     puts ARGV
   end
 
-  def self.find_arg_val(flag) # returns the next after a specified flag string, such as "-t"
-    return nil if ARGV.empty? # if there's no args, prevent returning of empty array
-
-    ARGV.each_index do |index|
-      if ARGV[index] == flag
+  def self.find_arg_val(flag)  # returns the next after a specified flag string, such as "-t"
+    return nil if ARGV.empty?  # if there's no args, prevent returning of empty array
+    ARGV.each_index do |index| # otherwise look for the arg
+      if ARGV[index] == flag   # if it's found, return the next thing (which, by convention, is the associated value)
         return ARGV[index+1]
       else
         return nil
@@ -19,5 +18,15 @@ module ArgParser
   def self.val_at_flag(flag) # just calls the function above
     find_arg_val(flag)
   end
+
+ def self.given_flag?(flag)
+   ARGV.each do |arg|
+     if arg == flag
+       return true
+     else
+       return false
+     end
+   end
+ end
 
 end
